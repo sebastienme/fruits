3f838576bcfbce9d6cdc07eb181eba5cdd1a3dfa
sebastien <sebastien.meslage@gmail.com>
2024-06-09 17:15:25 -0400

    add filters

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,15 +1,68 @@
 import styles from './Product.module.css'
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import cube from '../../assets/cube.png'
+import { Button } from '../Button/Button';
 
+const useProduct = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const { productId } = useParams();
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${productId}`)
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error("server error");
+                  }
+                return response.json()
+            })
+            .then((response) => setData(response))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }, [])
+
+    return {data, error,loading}
+}
 
 const Product = () => {
+    const { data, error, loading } = useProduct();
     const { productId } = useParams();
+
+    useEffect(() => {
+        if (data) {
+          console.log(data);
+        }
+    }, [data]);
+
+    if (loading) return <p>Ça load...</p>;
+    if (error) return <p>Une erreur réseau a été rencontrée</p>;
 
 
 
     return (
         <>
-        <div className={styles.big}>Product ID: {productId}</div>
+        <div className={styles.container}>
+            <div className={styles.image}>
+                <img src={data.image} />
+            </div>
+            <div className={styles.details}>
+                <div className={styles.title}>{data.title}</div>
+                <div>{data.category}</div>
+                <div className={styles.stocks}>
+                    <img src={cube} className={styles.cube}/>
+                    Disponible
+                </div>
+                <div className={styles.price}>{data.price}$</div>
+                <div> quantity</div>
+                <div>{data.description}</div>
+                <div className={styles.buttons}>
+                    <Button name='Achetez' />
+                    <Button name='Retirez du panier' />
+                </div>
+            </div>
+        </div>
         </>
     )
 }
--- src/component/Store/Store.jsx
+++ src/component/Store/Store.jsx
@@ -3,12 +3,24 @@ import Card from '../Cards/Cards'
 import styles from './Store.module.css'
 import { shuffle } from 'fast-shuffle'
 import { Link } from 'react-router-dom';
-import { Outlet } from "react-router-dom";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const useProducts = () => {
     const [data, setData] = useState(null);
+    const [categories, setCategories] = useState(null);
     const [error, setError] = useState(null);
+    const [errorCat, setErrorCat] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [loadingCat, setLoadingCat] = useState(true);
+    const [categoryPick, setCategoryPick] = useState(null);
+
+
+    /* TODO: faire un useEffect avec les filtres qui vient chercher les produits
+             de la categorie clique dans les filtres. 
+    */ 
+
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
@@ -23,40 +35,66 @@ const useProducts = () => {
             .finally(() => setLoading(false));
     }, [])
 
-    return {data, error,loading}
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products/categories')
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error("server error");
+                  }
+                return response.json()
+            })
+            .then((response) => setCategories(response))
+            .catch((error) => setErrorCat(error))
+            .finally(() => setLoadingCat(false));
+    }, [])
+
+
+    return {data, error,loading, categories, errorCat, loadingCat}
 }
 
 const Store = () => {
-    const { data, error, loading } = useProducts();
+    const { data, error,loading, categories, errorCat, loadingCat } = useProducts();
 
-    useEffect(() => {
-        if (data) {
-          console.log(data);
-        }
-    }, [data]);
-    
     if (loading) return <p>Ça load...</p>;
     if (error) return <p>Une erreur réseau a été rencontrée</p>;
 
+    if (loadingCat) return <p>Les filtres load...</p>;
+    if (errorCat) return <p>Une erreur réseau a été rencontrée</p>;
+
     const itemCount = data ? data.length : 0;
+
     
     return (
         <>
-            <div className={styles.store_title}>Produits<span>({itemCount})</span></div>
-            <div className={styles.products}>
-                {shuffle(data.map((item) => (
-                    <Link to={`${item.id}`} key={item.id}>
-                        <Card
-                            key={item.id}
-                            image={item.image}
-                            title={item.title}
-                            category={item.category}
-                            price={item.price}
-                        />
-                    </Link>
-                )))}   
+        <div className={styles.flex_container}>
+            <div className={styles.filtre_container}>
+                <div className={styles.filter_title}>Filtre</div>
+                <div className={styles.filter_items}>
+                    <FormGroup>
+                        {categories.map((item) => (
+                            <a key={item}><FormControlLabel control={<Checkbox />} label={item}/></a>
+                        ))}
+                    </FormGroup>
+                </div>
             </div>
-            <Outlet />
+            <div className={styles.right_container}>
+                <div className={styles.store_title}>Produits <span>({itemCount})</span></div>
+                <div className={styles.products}>
+                    {shuffle(data.map((item) => (
+                        <Link to={`${item.id}`} key={item.id}>
+                            <Card
+                                key={item.id}
+                                image={item.image}
+                                title={item.title}
+                                category={item.category}
+                                price={item.price}
+                            />
+                        </Link>
+                    )))}   
+                </div>
+            </div>
+        </div>
+
         </>
     )
 }
